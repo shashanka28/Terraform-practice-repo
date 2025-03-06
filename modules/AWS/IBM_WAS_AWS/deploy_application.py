178b229c17e9f38bc80109e73911292d0d2e25de
shashanka28 <106206851+shashanka28@users.noreply.github.com>
2025-03-06 15:36:08 -0500

    Update and rename deploy_application.py to deploy_java_application.py

--- modules/AWS/IBM_WAS_AWS/deploy_application.py
+++
@@ -1,9 +0,0 @@
-import sys
-from com.ibm.ws.scripting import *
-
-server = AdminConfig.getid('/Servers/' + sys.argv[0])
-app_name = "my_application"
-app_file_path = "/path/to/application.war"
-AdminApp.install(app_file_path, ['-server', server])
-
-AdminConfig.save()
---
+++ modules/AWS/IBM_WAS_AWS/deploy_java_application.py
@@ -0,0 +1,1 @@
+python script file which triggers, and deploy java WAR files in EC2 instances configured in here 
